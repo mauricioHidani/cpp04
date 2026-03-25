bccd0c0c0d9d4320fc1760f4ec0d29ce79fa7d20
Mauricio Mityo Hidani <mhidani@c1r7p4.42sp.org.br>
2026-03-25 15:29:58 -0300

    fix: builder WrongAnimal

--- ex00/src/WrongCat.cpp
+++ ex00/src/WrongCat.cpp
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 18:19:16 by mhidani           #+#    #+#             */
-/*   Updated: 2026/03/24 17:40:41 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/03/25 15:29:15 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ WrongCat::WrongCat(void): WrongAnimal() {
 	_type = "WrongCat";
 }
 
-WrongCat::WrongCat(const std::string& type) {
+WrongCat::WrongCat(const std::string& type): WrongAnimal() {
 	_type = type;
 }
 
-WrongCat::WrongCat(const WrongCat& other) {
+WrongCat::WrongCat(const WrongCat& other): WrongAnimal() {
 	_type = other.getType();
 }
 
